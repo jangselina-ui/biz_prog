65b1460cb975a84a4ff8d545609877b8bfdf6869
jangselina-ui <jang.selina@gmail.com>
2026-05-26 15:16:30 +0900

    Add files via upload

--- biz_st.py
+++ biz_st.py
@@ -1,8 +1,15 @@
 import streamlit as st #라이브러리 임포트
 
 #타이틀 텍스트 출력
-st.title('웹 어플 만들기 야루😊')
+st.title('웹 어플 만들기😊')
 
 "## 부제목에는 너구리🦝"
 
-"- 목록에는 곰돌이🐻"
+'# 웹 어플 만들기 히히😊'
+
+
+"""### Magic 적용
+
+- unordered item"""
+
+
